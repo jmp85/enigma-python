f326b9d6197a074db45f814c973c94dc6ad664c9
James <james_2002uk@hotmail.com>
2016-02-26 21:56:28 +0000

    First commit of stuff

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = "enigma",
+    version = "0.0.1",
+    author = "JMP",
+    author_email = "",
+    description = "",
+    license = "BSD",
+    keywords = "",
+    packages=["enigma", "tests"],
+    long_description="",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+)
