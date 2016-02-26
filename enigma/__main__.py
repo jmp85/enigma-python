01f563f6fc957532a27560ab709a29b060250ad1
James <james_2002uk@hotmail.com>
2016-02-26 22:53:48 +0000

    Added some travis stuff and some script stuff

---
+++ enigma/__main__.py
@@ -0,0 +1,38 @@
+
+class ceasar(object):
+
+    def encode(self, string):
+        return string[::-1]
+
+    def decode(self, string):
+        return string[::-1]
+
+def select_cipher(name):
+    table = {
+        "ceasar" : ceasar
+    }
+    return table[name]()
+
+def encipher():
+    from argparse import ArgumentParser
+    import sys
+
+    parser = ArgumentParser(description='Encipher a text file')
+    parser.add_argument(
+        '-c,--cipher', 
+        dest='cipher', 
+        choices=['ceasar'],
+        help='The cipher to use')
+    args = parser.parse_args()
+
+    cipher = select_cipher(args.cipher)
+
+    for line in sys.stdin.readlines():
+        print cipher.encode(line)
+        
+
+def decipher():
+    print "Deciphering"
+
+def decrypt():
+    print "Decrypting"
--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 from setuptools import setup
 
 setup(
-    name = "enigma",
-    version = "0.0.1",
-    author = "JMP",
-    author_email = "",
-    description = "",
-    license = "BSD",
-    keywords = "",
-    packages=["enigma", "tests"],
-    long_description="",
-    classifiers=[
+    name             = "enigma",
+    version          = "0.0.1",
+    author           = "JMP",
+    author_email     = "",
+    description      = "",
+    long_description = "",
+    license          = "BSD",
+    keywords         = "",
+    classifiers = [
         "Development Status :: 2 - Pre-Alpha",
     ],
+    packages  = [
+        "enigma", 
+        "tests"
+    ],
+    entry_points = {
+        'console_scripts' : [
+            'enigma-encipher = enigma.__main__:encipher',
+            'enigma-decipher = enigma.__main__:decipher',
+            'enigma-decrypt  = enigma.__main__:decrypt'
+        ]
+    },
 )
